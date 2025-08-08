0069ce80a4486145c14ee1708d2c9c26a6e540af
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-08-08 14:02:56 +0900

    project setting

---
+++ src/main/java/com/example/gapdori/GapdoriApplication.java
@@ -0,0 +1,13 @@
+package com.example.gapdori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapdoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GapdoriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gapdori/GapdoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gapdori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapdoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
